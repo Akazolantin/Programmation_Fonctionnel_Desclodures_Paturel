0ffcdc6b6cd24069124b85e0b6be3b83fb89bf50
Ronan Paturel <ronan.paturel@hei.yncrea.fr>
2021-10-26 17:53:59 +0200

    Unversioned files

--- src/main/scala/com/hei/fp/functions/Main.scala
+++ src/main/scala/com/hei/fp/functions/Main.scala
@@ -1,5 +1,6 @@
 package com.hei.fp.functions
 
+import breeze.numerics.sqrt
 import play.api.libs.json.{JsValue, Json}
 import scalaj.http.{Http, HttpResponse}
 
@@ -10,7 +11,7 @@ object Main {
   def main(args: Array[String]): Unit = {
 
     val api : CallAPI = new CallAPI()
-    val liste1 = api.callAPI()
+    val liste1 = api.callAPI(100)
     //println(liste1(0)(0))
     //println(liste1(0)(4))
 
@@ -25,21 +26,35 @@ object Main {
         }
         somme = somme / 20
         res += somme
-        println(somme)
       }
       val resList = res.toList
       return resList
     }
-    //var liste = Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21)
-    println(moyenne_mobile(liste1))
+
+    println(moyenne_mobile(liste1)(0))
 
 
     def RSI(list: Array[Int]): Unit = {
 
     }
 
-    def Bollinger(list: Array[Int]): Unit = {
-
+    //Bollinger avec la moyenne mobile sur 20 jours
+    def Bollinger(list: ArrayBuffer[Array[Any]]): List[Double] = {
+      var somme : Double = 0.0;
+      var res = new ListBuffer[Double]()
+      for (i <- 20 to list.length){
+        for (n <- i-20 to i-1){
+          somme = somme + ((list(n)(4).asInstanceOf[Double]-moyenne_mobile(liste1)(i-20))*(list(n)(4).asInstanceOf[Double]-moyenne_mobile(liste1)(i-20)))
+        }
+        somme = somme / 20
+        somme = sqrt(somme)
+        somme = somme * 2
+        res += somme
+      }
+      val resList = res.toList
+      return resList
     }
+
+    println(Bollinger(liste1))
   }
 }

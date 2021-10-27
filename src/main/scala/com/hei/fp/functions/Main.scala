c1c6cef684e7e96eb42a7451b7061d3b778847fe
Ronan Paturel <ronan.paturel@hei.yncrea.fr>
2021-10-27 09:13:08 +0200

    Bollinger + moyenne mobile pondérée

--- src/main/scala/com/hei/fp/functions/Main.scala
+++ src/main/scala/com/hei/fp/functions/Main.scala
@@ -1,5 +1,6 @@
 package com.hei.fp.functions
 
+import breeze.math.i
 import breeze.numerics.sqrt
 import play.api.libs.json.{JsValue, Json}
 import scalaj.http.{Http, HttpResponse}
@@ -11,7 +12,7 @@ object Main {
   def main(args: Array[String]): Unit = {
 
     val api : CallAPI = new CallAPI()
-    val liste1 = api.callAPI(100)
+    val liste1 = api.callAPI(21)
     //println(liste1(0)(0))
     //println(liste1(0)(4))
 
@@ -31,16 +32,13 @@ object Main {
       return resList
     }
 
-    println(moyenne_mobile(liste1)(0))
+    println(liste1.length)
+    println(moyenne_mobile(liste1))
 
 
-    def RSI(list: Array[Int]): Unit = {
-
-    }
-
-    //Bollinger avec la moyenne mobile sur 20 jours
+    //Bollinger avec la moyenne mobile sur 20 jours (la valeur on fait +/- la valeur close pour faire un encadrement faire 2 fonctions ? pour valeur haute/basse ?
     def Bollinger(list: ArrayBuffer[Array[Any]]): List[Double] = {
-      var somme : Double = 0.0;
+      var somme : Double = 0.0
       var res = new ListBuffer[Double]()
       for (i <- 20 to list.length){
         for (n <- i-20 to i-1){
@@ -51,10 +49,39 @@ object Main {
         somme = somme * 2
         res += somme
       }
+
       val resList = res.toList
       return resList
     }
 
-    println(Bollinger(liste1))
+    //println(Bollinger(liste1))
+
+
+    def moyenne_mobile_pondérée(list: ArrayBuffer[Array[Any]]): List[Double] = {
+      var somme1 : Double = 0.0
+      var somme2 : Double = 0.0
+      var res = new ListBuffer[Double]()
+      var i : Int = 20
+      var sommeFin : Double = 0.0
+      for (r <- 1 to 20){
+        somme2 = somme2 + r
+      }
+      for (k <- 20 to list.length){
+        for (n <- 0 to 19){
+          i=i-1
+          somme1 = somme1 + (list(n+k-20)(4).asInstanceOf[Double] * i)
+        }
+        i=0
+        sommeFin = somme1 / somme2
+        res += sommeFin
+        somme1 = 0
+      }
+      val resList = res.toList
+      return resList
+    }
+
+    //println(moyenne_mobile_pondérée(liste1))
+    //println(liste1(0)(0))
+    //println(liste1(0)(4))
   }
 }
